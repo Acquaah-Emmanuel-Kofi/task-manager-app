336c25152d41155d3df816aa7c9c913dd885eaeb
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-05-16 23:17:26 +0000

    feat: create task schema validator

---
+++ apps/backend/src/validators/taskValidator.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const createTaskSchema = z.object({
+  user_id: z.number(),
+  title: z.string().min(1, "Title is required"),
+  description: z.string().optional(),
+  due_date: z.string().optional(),
+  status: z.enum(["pending", "in-progress", "completed"]).optional(),
+  priority: z.enum(["low", "normal", "high"]).optional(),
+});
+
+export const updateTaskSchema = createTaskSchema.partial();
