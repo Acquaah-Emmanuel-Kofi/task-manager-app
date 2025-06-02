e5318378852c58ecaf5f528c770fb9a4cdc83969
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-06-02 02:21:20 +0000

    feat: use tanstack to fetch tasks

--- apps/frontend/hooks/useTasks.ts
+++ apps/frontend/hooks/useTasks.ts
@@ -1,34 +1,14 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { hanldeApiError } from "@/lib/utils";
 import { ITask, ITaskResponse } from "@/interfaces/tasks";
 import api from "@/lib/axios";
 import { ApiResponse } from "@/interfaces/common";
+import { useQuery } from "@tanstack/react-query";
 
-export function useTasks() {
-  const [tasks, setTasks] = useState<ITask[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<null | string>(null);
-  const router = useRouter();
-
-  const fetchTasks = async () => {
-    try {
-      setLoading(true);
+export const useTasks = () => {
+  return useQuery<ITask[], Error>({
+    queryKey: ["tasks"],
+    queryFn: async () => {
       const { data }: ApiResponse<ITaskResponse> = await api.get("/tasks");
-      setTasks(data.data);
-    } catch (err: unknown) {
-      setError("Failed to fetch tasks");
-      hanldeApiError(err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, [router]);
-
-  return { tasks, loading, error, refetch: fetchTasks };
-}
+      return data.data;
+    },
+  });
+};
