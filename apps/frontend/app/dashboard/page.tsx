2a3f33dcdd5fd12581e99872c5e68e321e3d8658
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-06-08 18:22:05 +0000

    fix: data fetching before component mounts

--- apps/frontend/app/dashboard/page.tsx
+++ apps/frontend/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { useTasks } from "@/hooks/useTasks";
 import { DataTable } from "./components/data-table";
 import { columns } from "./components/columns";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
@@ -16,28 +17,25 @@ export default function DashboardPage() {
 }
 
 function Tasks() {
-  const { data: tasks, isLoading, error } = useTasks();
+  const { data: tasks, isLoading, error, refetch } = useTasks();
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
 
   if (error) return <div>Something went wrong. {error.message}</div>;
 
   return (
-    <>
-      <div className="h-full flex-1 flex-col space-y-8 p-8">
-        <div className="flex items-center justify-between space-y-2">
-          <div>
-            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
-            <p className="text-muted-foreground">
-              Here&apos;s a list of your tasks for this month!
-            </p>
-          </div>
-          {/* <div className="flex items-center space-x-2">
-            <UserNav />
-          </div> */}
+    <div className="h-full flex-1 flex-col space-y-8 p-8">
+      <div className="flex items-center justify-between space-y-2">
+        <div>
+          <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+          <p className="text-muted-foreground">
+            Here&apos;s a list of your tasks!
+          </p>
         </div>
-        {tasks && tasks?.length > 0 && (
-          <DataTable data={tasks} columns={columns} loading={isLoading} />
-        )}
       </div>
-    </>
+      <DataTable data={tasks ?? []} columns={columns} loading={isLoading} />
+    </div>
   );
 }
